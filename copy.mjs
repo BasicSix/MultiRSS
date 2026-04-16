107cf48da9a1514862cc38bfb3c19ebf2da8047f
Zhit Chan <132313725+BasicSix@users.noreply.github.com>
2026-04-16 15:33:03 +0800

    Update copy.mjs

--- copy.mjs
+++ copy.mjs
@@ -1,13 +1,15 @@
 import path from "path";
 import fs from "fs-extra";
 
-
 async function copy() {
-  const exampleRoot = path.resolve("examples");
+  // 核心修改：将路径从 examples 指向 channels
+  const exampleRoot = path.resolve("channels"); 
   const exampleFolders = await fs.readdir(exampleRoot);
+  
   await Promise.all(exampleFolders.map(exampleFolder => {
     const copyFrom = path.join(exampleRoot, exampleFolder, "public");
-    const copyTo = path.resolve(exampleRoot, `../dist/${exampleFolder}`);
+    // 拷贝到 dist 目录下，保持频道名称
+    const copyTo = path.resolve("dist", exampleFolder);
     return fs.copy(copyFrom, copyTo);
   }));
 } 
